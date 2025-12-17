ec74d62b35ec1e5f899ae0d369ba3e45805b28de
KierankThompson <thompson.kierank@gmail.com>
2025-12-17 05:24:52 -0500

    problem 1

--- problem1.py
+++ problem1.py
@@ -1,6 +1,6 @@
 dial = 50
 
-password = 0
+password1 = 0
 
 password2 = 0
 
@@ -9,27 +9,26 @@ with open("problem1.txt", 'r') as f:
     for line in lines:
         line = line.rstrip("\n")
         modify = int(line[1:])
-        prevDial = dial
         password2 += modify // 100
         modify %= 100
         if line[0] == 'L':
             if modify > dial:
-                password2 += 1
                 dial = 100 - (modify - dial)
+                if modify + dial > 100:
+                    password2 += 1
             else:
                 dial -= modify
+            
         else:
-            if modify + dial >= 100:
-                password2 += 1
+            if dial + modify > 100:
+                    password2 += 1
             dial = (modify + dial) % 100
             
         
         if dial == 0:
-            password += 1
-            
-        if dial >= 100:
-            print(f"error: {prevDial=} {line=}")
-print(password)
-print(password2)
+            password1 += 1
+
+print(f"part 1: {password1}")
+print(f"part2: {password2 + password1}")
 
 
