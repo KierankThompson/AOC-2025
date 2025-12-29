c77f6048c7865dd26318993601d5d2c825a6ad98
KierankThompson <thompson.kierank@gmail.com>
2025-12-29 07:57:51 -0500

    problem 4

---
+++ problem4-1.py
@@ -0,0 +1,20 @@
+with open("problem4.txt", 'r') as f:
+    lines = f.read().splitlines()
+floorMap = set()
+for i, line in enumerate(lines):
+    for j, char in enumerate(line):
+        if char == '@':
+            floorMap.add((i,j))
+
+accessibleRolls = 0
+directions = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+for i,j in floorMap:
+        rollCount = 0
+        for v,h in directions:
+            if (i+v,j+h) in floorMap:
+                rollCount += 1
+        if rollCount >= 4:
+            continue
+        accessibleRolls += 1
+print(accessibleRolls)
+
